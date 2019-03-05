e5b220d58d3b62f1c7e5a0a9eb758cf7251c355c
Seth Bernard <sethbernard7@gmail.com>
2019-03-05 12:59:36 -0500

    make tweet button work

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+document.addEventListener("DOMContentLoaded", getNewQuote);
+
 let url =
   "https://cors-anywhere.herokuapp.com/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-let text, author;
+let quote, author;
 
 async function getNewQuote() {
   let response = await fetch(url, { cache: "no-cache" });
   let data = await response.json();
-  text = data[0].content;
-  newAuthor = data[0].title;
-  document.getElementById("text").innerText = text;
-  document.getElementById("author").innerText = newAuthor;
+  quote = data[0].content;
+  author = data[0].title;
+  document.getElementById("text").innerText = quote;
+  document.getElementById("author").innerText = author;
 }
+
+const sendTweet = () => {
+  let twitterUrl = (document.getElementById("tweet-quote").href =
+    "https://twitter.com/intent/tweet?text=");
+  let tweet = twitterUrl + encodeURI(quote) + " -" + encodeURI(author);
+  window.open(tweet);
+};
 
 let quoteBtn = document.getElementById("new-quote");
 quoteBtn.addEventListener("click", getNewQuote);
+
+let tweetBtn = document.getElementById("tweet-quote");
+tweetBtn.addEventListener("click", sendTweet);
