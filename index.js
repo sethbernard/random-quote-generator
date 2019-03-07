2a5714dcd0c13b74053f8b003713db811347ac44
Seth Bernard <sethbernard7@gmail.com>
2019-03-07 18:23:25 -0500

    create server with express

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
