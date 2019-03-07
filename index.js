a44017a26abb46a6a7eb5cd1e11061d69b8e43b2
Seth Bernard <sethbernard7@gmail.com>
2019-03-07 18:58:15 -0500

    change syntax of port constant

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
